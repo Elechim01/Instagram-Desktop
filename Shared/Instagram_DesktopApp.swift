9b9822a38407e52a5dfbf528243208c82610f349
Elechim01 <mikymanny015@gmail.com>
2021-02-16 17:06:09 +0100

    Initial Commit

---
+++ Shared/Instagram_DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_DesktopApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
